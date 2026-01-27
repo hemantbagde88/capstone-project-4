ee371c6a1a6994bb1cf6ff411a4839fc125f009e
Hemant <hemantbagde88@gmail.com>
2026-01-27 22:19:24 +0530

    model building is completed

---
+++ src/model_building.py
@@ -0,0 +1,138 @@
+import os 
+import numpy as np
+import pandas as pd
+import pickle
+import logging 
+from sklearn.ensemble import RandomForestClassifier
+import yaml
+
+# ensure log directory is exist 
+log_dir = 'logs'
+os.makedirs(log_dir, exist_ok=True)
+
+#logging configuration 
+logger = logging.getLogger("model_building")
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir, 'model_building.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_params(param_path: str) -> dict:
+    """Load parameters from YAML file."""
+    try:
+        with open(param_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s' , param_path)
+        return params
+    except FileExistsError:
+        logger.error('file not found: %s', param_path)
+        raise 
+    except yaml.YAMLError as e:
+        logger.error("YAML error: %s", e)
+        raise 
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise 
+
+
+def load_data(file_path: str) -> pd.DataFrame:
+    """
+    Load data from a csv file.
+    :param file_path: path to the CSV file
+    :return: Loaded DataFrame 
+    """
+    try: 
+        df = pd.read_csv(file_path)
+        logger.debug('Data loaded from %s with shape %s', file_path, df.shape)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the csv file: %s', e)
+        raise
+    except FileNotFoundError as e:
+        logger.error('file not found %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexcepted error ocuured while loading the data: %s', e)
+        raise
+
+def train_model(X_train: np.ndarray, y_train: np.ndarray, params: dict) ->RandomForestClassifier:
+    """
+    Train the RandomForest model.
+
+    :param X_train: training features 
+    :param y_train: Training Labels 
+    :param params: Dictionary of the hyperparameters
+    :return: Trained RandomForestClassifier
+
+    """
+
+    try:
+        if X_train.shape[0] != y_train.shape[0]:
+            raise ValueError("the number of samples in X_train and y_train must be same.")
+        
+        logger.debug("intializing RandomForest_Model with parameters: %s", params)
+        clf = RandomForestClassifier(n_estimators=params['n_estimators'], random_state=params['random_state'])
+
+        logger.debug("Model training started with %d samples", X_train.shape[0])
+        clf.fit(X_train,y_train)
+        logger.debug("model training completed")
+
+        return clf
+    except ValueError as e:
+        logger.error("Error during model training: %s", e)
+        raise
+
+
+def save_model(model, file_path: str) -> None:
+    """
+     save the trained model to save_model
+    
+    :param model: trained model object
+    :param file_path: path to save the model 
+    :type file_path: str
+    """
+
+    try: 
+        #ensure the directory exists
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        with open(file_path, 'wb') as file:
+            pickle.dump(model, file)
+        logger.debug('model save to %s', file_path)
+    except FileNotFoundError as e:
+        logger.error("file not found %s",e)
+        raise
+    except Exception as e:
+        logger.error("error occured while saving the model %s", e)
+        raise
+
+def main():
+    try:
+        params = load_params('params.yaml')['model_building']
+        train_data = load_data('./data/processed/train_tfidf.csv')
+        X_train = train_data.iloc[:, :-1].values
+        y_train = train_data.iloc[:, -1].values
+
+        clf = train_model(X_train, y_train, params)
+        
+        model_save_path = 'models/model.pkl'
+        save_model(clf, model_save_path)
+
+    except Exception as e:
+        logger.error('Failed to complete the model building process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()  
